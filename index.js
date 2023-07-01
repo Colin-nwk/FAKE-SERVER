a2b5e262361c837f241b09837414089109ce0460
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-07-01 23:27:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+ 
+import vegans from "./routes/vegans.js"
 
-import { readdirSync } from "fs";
-import { parse } from "path";
+//import { readdirSync } from "fs";
+//import { parse } from "path";
 
 import { errorHandler } from "./middlewares/errorHandler.js";
 
@@ -15,12 +17,16 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-readdirSync("./routes").map(async (file) => {
-  const routePath = `./routes/${file}`;
-  const { default: route } = await import(routePath);
-  const routeName = `/${parse(file).name}`;
-  app.use(routeName, errorHandler, route);
-});
+//readdirSync("./routes").map(async (file) => {
+//  const routePath = `./routes/${file}`;
+//  const { default: route } = await import(routePath);
+//  const routeName = `/${parse(file).name}`;
+ // app.use(routeName, errorHandler, route);
+//});
+
+
+app.use("/vegans", errorHandler, vegans);
+
 
 app.get("/", (req, res) => {
   res.send("main page 1.0.0");
