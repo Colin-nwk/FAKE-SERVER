3af65d031c2d4b362848de4c60eb91cd2a769c68
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-07-01 20:37:11 +0100

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { readdirSync } from "fs";
+import { join, parse } from "path";
+
+import { errorHandler } from "./middlewares/errorHandler.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+readdirSync("./routes").map(async (file) => {
+  const routePath = `./routes/${file}`;
+  const { default: route } = await import(routePath);
+  const routeName = `/${parse(file).name}`;
+  app.use(routeName, errorHandler, route);
+});
+
+app.get("/", (req, res) => {
+  res.send("main page 1.0.0");
+});
+
+app.listen(port, () => {
+  console.info("app runing at port : " + port);
+});
---
+++ routes/cakes.js
@@ -0,0 +1,67 @@
+import { Router } from "express";
+
+import { readFile } from "fs/promises";
+
+const router = Router();
+let items = [];
+
+async function loadItems() {
+  try {
+    const data = await readFile("./data/cakesdetails.json", "utf8");
+    items = JSON.parse(data);
+    // console.log("Data loaded successfully");
+  } catch (error) {
+    // console.error("Error reading or parsing JSON:", error);
+    throw new Error("Failed to load items");
+  }
+}
+
+router.get("/", (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const paginatedItems = items.slice(startIndex, endIndex);
+
+  res.json({
+    totalItems: items.length,
+    totalPages: Math.ceil(items.length / limit),
+    currentPage: parseInt(page),
+    items: paginatedItems,
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  const itemId = req.params.id;
+  const item = items.find((item) => item.id === itemId);
+
+  if (item) {
+    res.json(item);
+  } else {
+    const error = new Error("Item not found");
+    error.status = 404;
+    next(error);
+  }
+});
+
+router.get("/search", (req, res, next) => {
+  const { q, page = 1, limit = 10 } = req.query;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const searchResults = items.filter((item) =>
+    item.title.toLowerCase().includes(q.toLowerCase())
+  );
+
+  const paginatedResults = searchResults.slice(startIndex, endIndex);
+
+  res.json({
+    totalItems: searchResults.length,
+    totalPages: Math.ceil(searchResults.length / limit),
+    currentPage: parseInt(page),
+    items: paginatedResults,
+  });
+});
+
+loadItems();
+
+export default router;
