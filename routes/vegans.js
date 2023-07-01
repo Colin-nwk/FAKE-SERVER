007593b3eeafc6a2cd2a00049a661f2355158303
Collins Nwoko C <collinsnwoko05@gmail.com>
2023-07-01 22:42:00 +0100

    vegan route

--- routes/vegans.js
+++ routes/vegans.js
@@ -3,18 +3,70 @@ import { Router } from "express";
 import { readFile } from "fs/promises";
 
 const router = Router();
-let items = [];
+// let items = [];
 
-async function loadItems() {
-  try {
-    const data = await readFile("./data/vegans.json", "utf8");
-    items = JSON.parse(data);
-    console.log("Data loaded successfully");
-  } catch (error) {
-    console.error("Error reading or parsing JSON:", error);
-    throw new Error("Failed to load items");
-  }
-}
+// async function loadItems() {
+//   try {
+//     const data = await readFile("./data/vegans.json", "utf8");
+//     items = JSON.parse(data);
+//     console.log("Data loaded successfully");
+//   } catch (error) {
+//     console.error("Error reading or parsing JSON:", error);
+//     throw new Error("Failed to load items");
+//   }
+// }
+
+const items = [
+  {
+    id: "1",
+    title:
+      "Dark chocolate bark with sea salt, rosemary, pistachios and candied citrus",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/1.jpg",
+  },
+  {
+    id: "2",
+    title: "Pressure cooker refried beans",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/2.jpg",
+  },
+  {
+    id: "3",
+    title: "Beetroot, spinach and coconut curry",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/3.jpg",
+  },
+  {
+    id: "4",
+    title: "Traditional pretzels",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/4.jpg",
+  },
+  {
+    id: "5",
+    title: "Homemade crispy chilli oil",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/5.jpg",
+  },
+  {
+    id: "6",
+    title: "Ultimate vegan lasagne",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/6.jpg",
+  },
+  {
+    id: "7",
+    title: "Vegan Victoria sponge cake",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/7.jpg",
+  },
+  {
+    id: "8",
+    title: "Vegan quiche",
+    difficulty: "Easy",
+    image: "https://apipics.s3.amazonaws.com/vegan_api/8.jpg",
+  },
+];
 
 router.get("/", (req, res, next) => {
   try {
@@ -34,10 +86,10 @@ router.get("/", (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   try {
     const itemId = req.params.id;
-    const item = await items.find((item) => item.id === itemId);
+    const item = items.find((item) => item.id === itemId);
 
     if (item) {
       return res.json(item);
